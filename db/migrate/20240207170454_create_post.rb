abb4feca2166a6eaddeede5dbd56cacbf602b7c6
elainefang <elfang16@gmail.com>
2024-02-07 12:17:55 -0500

    Add posts

---
+++ db/migrate/20240207170454_create_post.rb
@@ -0,0 +1,11 @@
+class CreatePost < ActiveRecord::Migration[7.1]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :content
+      t.string :image_url
+      t.references :user
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_033244) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_07_170541) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
