cba8ce3f2418bfee4e8a31be534ad3a87f804318
elainefang <elfang16@gmail.com>
2024-02-07 23:24:10 -0500

    Add likes to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_07_170541) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_08_042009) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes", default: 0
   end
 
   create_table "users", force: :cascade do |t|
