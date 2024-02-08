f3474eca86b61c055b26f703f896b12e5bd4effa
elainefang <elfang16@gmail.com>
2024-02-07 23:47:24 -0500

    Reset schema

--- db/schema.rb
+++ db/schema.rb
@@ -23,9 +23,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_042820) do
   end
 
   create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "image_url"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "likes", default: 0
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
