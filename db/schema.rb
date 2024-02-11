238ddc10c2bdd2a019353181d2f9fd563d22e992
elainefang <elfang16@gmail.com>
2024-02-11 01:28:53 -0500

    Add trackable options for Devise user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_08_042820) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_11_062720) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_042820) do
     t.boolean "age_over_thirteen"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
