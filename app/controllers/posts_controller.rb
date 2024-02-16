6a2e09977d5691026a702e47fcfbabaf5841ceb0
elainefang <elfang16@gmail.com>
2024-02-15 22:44:32 -0500

    Add post controller actions and icon

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,24 @@
 class PostsController < ActionController::Base
     layout 'application'
+    before_action :load_user
 
     def index; end
+
+    def show; end
+
+    def new; end
+
+    def create; end
+
+    def edit; end
+
+    def update; end
+
+    def destroy; end
+
+    private
+
+    def load_user
+        @user = User.find(params[:user_id])
+    end
 end
