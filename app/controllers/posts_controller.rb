af3535ed942ab18ad570878dfecaf6980d7ddaa8
elainefang <elfang16@gmail.com>
2024-02-20 00:00:26 -0500

    Add pry gem and page for new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,6 +19,6 @@ class PostsController < ActionController::Base
     private
 
     def load_user
-        @user = User.find(params[:user_id])
+        @user = User.find(params[:account_id])
     end
 end
