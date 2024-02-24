8927c2818d72d9426cd55306a79856c0097f9cdf
elainefang <elfang16@gmail.com>
2024-02-24 01:08:29 -0500

    Add active storage images to user and post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :posts, dependent: :destroy
+  has_one_attached :avatar, dependent: :destroy
   validates_presence_of :email
   validates_presence_of :encrypted_password
   validates_presence_of :first_name
