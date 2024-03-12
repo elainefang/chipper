4ecbc43fb64fe016b0f7f28a48841ebbedd44b17
elainefang <elfang16@gmail.com>
2024-03-12 14:16:40 -0400

    Add multiple images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -32,6 +32,6 @@ class PostsController < ApplicationController
 	end
 
 	def post_params
-		params.require(:post).permit(:title, :content, :images)
+		params.require(:post).permit(:title, :content, images: [])
 	end
 end
