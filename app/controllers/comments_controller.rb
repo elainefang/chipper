2ab1b3aca958dc7e9ff1f6a502503205e8a0e325
elainefang <elfang16@gmail.com>
2024-03-12 16:17:43 -0400

    Add comments to post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,12 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    @comment.user = current_user
+    if @comment.save
+      redirect_to post_path(@post), notice: 'Comment was published.'
+    else
+      redirect_to post_path(@post), error: 'Comment submission failed.'
+    end
   end
 
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_185543) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_12_200942) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,7 +47,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_185543) do
     t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -84,4 +86,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_185543) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
 end
