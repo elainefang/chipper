0cebf7adb91296bd2e8d944da434d11aac732b4c
elainefang <elfang16@gmail.com>
2024-03-14 01:56:07 -0400

    Add validations for uploads

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,22 @@ class Post < ApplicationRecord
     has_many :comments
     has_many_attached :images, dependent: :destroy
 
+    validate :acceptable_images
+
     scope :with_comments, -> { left_joins(:comments).select('posts.*, COUNT(comments.id) AS comment_count').group('posts.id') }
+
+    def acceptable_images
+      return unless images.attached?
+
+    	images.each do |image|
+				unless image.byte_size <= 2.megabyte
+					errors.add(:images, 'too big')
+				end
+
+				acceptable_types = ["image/jpeg", "image/png"]
+				unless acceptable_types.include?(main_image.content_type)
+					errors.add(:images, "must be a JPEG or PNG")
+				end
+			end
+    end
 end
